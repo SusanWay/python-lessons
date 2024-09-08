083e9cf4dafdb5ad578bd4b2de41b739a5d5e24b
SusanWay <zaev_2030@mail.ru>
2024-09-08 15:22:05 +0300

    Создал начальную структуру хранения уроков

---
+++ 2. Типы данных/Решения/Task-1.py
@@ -0,0 +1,75 @@
+num1=8
+num2=6
+sum_result=num1+num2
+print("Сумма",num1,"и",num2,"равна",sum_result)
+
+num1=8
+num2=4
+sum_result=num1*num2
+print("произведение чисел",num1,"и",num2,"равна",sum_result)
+
+num1=9
+num2=2
+sum_result=num1-num2
+print("разность чисел",num1,"и",num2,"равна",sum_result)
+
+num1=65
+num2=5
+sum_result=num1/num2
+print("Частное чисел",num1,"и",num2,"равна",sum_result)
+
+num1=3.14
+num2=2
+num3=6 #диаметр
+sum_result=num1*num2*num3
+print("диаметр круга равен",sum_result)
+
+string = 'куку'
+result = string * 2
+print(result)
+
+num1=5 #градусов цельсия
+num2=1.8
+num3=32
+sum_result=num1*num2+num3
+print(num1," градусов Цельсия равны",sum_result,"градусам Фаренгейта")
+
+num1=5 #A
+num2=9 #B
+num3=3
+num4=2
+sum_result=num1+num2*num3-num4
+print("Результат выражения a + b * 3 - 2 равен" ,sum_result)
+
+num1=5
+num2=3
+num3=3
+num4=2
+sum_result=(num1+num2+num3+num4)/4
+print("Средняя оценка равна",sum_result)
+
+num1=5 #километры
+num2=1.609
+sum_result=num1/num2
+print(num1,"километров равны",sum_result,"мили")
+
+num1=4 #ребро куба
+sum_result=num1*num1*num1
+print("Объем куба с ребром",num1,"равен",sum_result)
+
+num1=1
+num2=2
+num3=3
+num4=4
+num5=5
+sum_result1=num1*num1
+sum_result2=num2*num2
+sum_result3=num3*num3
+sum_result4=num4*num4
+sum_result5=num5*num5
+print("Квадрат числа", num1,"равен",sum_result1,
+      "Квадрат числа", num2,"равен",sum_result2,
+      "Квадрат числа", num3,"равен",sum_result3,
+      "Квадрат числа", num4,"равен",sum_result4,
+      "Квадрат числа", num5,"равен",sum_result5)
+
